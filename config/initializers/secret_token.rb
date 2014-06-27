f9ebfc18310937f42cda0ce5c43c827fe052de5f
isheeta.dabhi-BTC <isheeta.dabhi@beaverlogic.com>
2014-06-27 15:33:08 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyProject::Application.config.secret_key_base = 'd3251dc0a6ab8b407f8c16dd374ce705c474082640f0627a6f1d684c5305a787076cb5f31fd1d86abe818d3f431b4445dc13e99cbe967a00df3b37de463f109b'
